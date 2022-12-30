89faa3f67f65b218e29fae8a2cb188069d31820a
masily <masily365@gmail.com>
2022-12-30 19:53:11 +0800

    添加sha256

--- Casks/navicat-premium-cn.rb
+++ Casks/navicat-premium-cn.rb
@@ -1,6 +1,6 @@
 cask "navicat-premium-cn" do
   version "16.1.4"
-  sha256 :no_check
+  sha256 "d0dfcd4897796067f22d2ac8d8865c5079cae2f29601cc940870e5e8354a43bf"
   url "https://download.navicat.com.cn/download/navicat#{version.major_minor.no_dots}_premium_cs.dmg"
   name "Navicat Premium"
   desc "Database administration and development tool"
